359f6de9e0344d503e866ce4d81048d467341763
Ishitta Sarkar <ishittas4@gmail.com>
2026-06-16 04:38:30 +0530

    Add basic DNA sequence analysis

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello Bioinformatics")
+print("Bioinformatics Core Toolkit")
+print("=" * 35)
+
+dna_sequence = "ATGCGTACCGTAGCTAGC"
+
+print("DNA Sequence:", dna_sequence)
+print("Length:", len(dna_sequence))
+
+gc_count = dna_sequence.count("G") + dna_sequence.count("C")
+gc_content = (gc_count / len(dna_sequence)) * 100
+
+print("GC Content:", round(gc_content, 2), "%")
+print("RNA Transcript:", dna_sequence.replace("T", "U"))
