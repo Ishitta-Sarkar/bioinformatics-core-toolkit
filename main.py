3568dd73fe226e334fc522d6712289f0408e360e
Ishitta Sarkar <ishittas4@gmail.com>
2026-06-16 04:45:32 +0530

    Use DNA tools module

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-print("Bioinformatics Core Toolkit")
-print("=" * 35)
+from src.dna_tools import DNATools
 
-dna_sequence = "ATGCGTACCGTAGCTAGC"
+sequence = DNATools("ATGCGTACCGTAGCTAGC")
 
-print("DNA Sequence:", dna_sequence)
-print("Length:", len(dna_sequence))
-
-gc_count = dna_sequence.count("G") + dna_sequence.count("C")
-gc_content = (gc_count / len(dna_sequence)) * 100
-
-print("GC Content:", round(gc_content, 2), "%")
-print("RNA Transcript:", dna_sequence.replace("T", "U"))
+print("Length:", sequence.length())
+print("GC Content:", sequence.gc_content())
+print("RNA:", sequence.transcribe())
