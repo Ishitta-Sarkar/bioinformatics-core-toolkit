e9a86e548abc4347db8bd307407b18b4c2074a7c
Ishitta Sarkar <ishittas4@gmail.com>
2026-06-16 05:30:52 +0530

    Update main program with advanced DNA analysis

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from src.dna_tools import DNATools
 
-sequence = DNATools("ATGCGTACCGTAGCTAGC")
+dna = DNATools("ATGTTTAAAGGG")
 
-print("Length:", sequence.length())
-print("GC Content:", sequence.gc_content())
-print("RNA:", sequence.transcribe())
+print("Bioinformatics Core Toolkit")
+print("=" * 35)
+
+print("DNA Sequence:", dna.sequence)
+print("Length:", dna.length())
+print("GC Content:", dna.gc_content(), "%")
+print("RNA Transcript:", dna.transcribe())
+print("Reverse Complement:", dna.reverse_complement())
+print("Protein Translation:", dna.translate_to_protein())
+
+reference = "ATGCGTACCGTAGCTAGC"
+sample = "ATGAGTACCGTGGCTAGC"
+
+mutations = DNATools.detect_mutations(reference, sample)
+
+print("\nMutation Detection")
+print("-" * 35)
+
+for mutation in mutations:
+    print(
+        "Position:",
+        mutation["position"],
+        "| Reference:",
+        mutation["reference"],
+        "| Sample:",
+        mutation["sample"]
+    )
